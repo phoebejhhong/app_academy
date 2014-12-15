4bdfb693d2d921386167367473b30e0c0b024b1f
Phoebe Hong <phoebejhhong@gmail.com>
2014-12-15 00:05:55 -0800

    finished

--- w1/word_chains.rb
+++ w1/word_chains.rb
@@ -20,7 +20,41 @@ class WordChainer
     end
     adjecent_words
   end
+
+  def run(source, target)
+    @all_seen_words = { source=>nil }
+    @current_words = [source]
+    until @current_words.empty?
+      @new_current_words = []
+      @current_words.each do |current_word|
+        explore_current_word(current_word)
+        @current_words = @new_current_words
+      end
+    end
+    build_path(source, target)
+  end
+
+  def explore_current_word(current_word)
+    adjecent_words(current_word).each do |adj_word|
+      next if @all_seen_words.include?(adj_word)
+      @new_current_words << adj_word
+      @all_seen_words[adj_word] = current_word
+    end
+  end
+
+  def build_path(source, target)
+    path = []
+    prev_word = @all_seen_words[target]
+    until prev_word.nil?
+      path << prev_word.dup
+      prev_word = @all_seen_words[prev_word]
+    end
+    path.reverse << target
+
+  end
+
+
 end
 
 wc = WordChainer.new('dictionary.txt')
-p wc.adjecent_words('word')
+p wc.run('in', 'to')
